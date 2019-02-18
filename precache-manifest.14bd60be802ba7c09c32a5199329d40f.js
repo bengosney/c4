369cbcf6e57d9b4d558b47ce78f1637b2da71adb
Ben Gosney <ben@evosite.co.uk>
2019-02-18 13:51:56 +0000

    Updates

---
+++ precache-manifest.14bd60be802ba7c09c32a5199329d40f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e2d1e3642b2104b542e",
+    "url": "/c4/static/css/main.67c675c4.chunk.css"
+  },
+  {
+    "revision": "1e2d1e3642b2104b542e",
+    "url": "/c4/static/js/main.1e2d1e36.chunk.js"
+  },
+  {
+    "revision": "8315106634bf8c630fb3",
+    "url": "/c4/static/js/1.83151066.chunk.js"
+  },
+  {
+    "revision": "4e547ec85ea040c46a41",
+    "url": "/c4/static/js/runtime~main.4e547ec8.js"
+  },
+  {
+    "revision": "2f632769e65906e16de651c894e6e695",
+    "url": "/c4/index.html"
+  }
+];
